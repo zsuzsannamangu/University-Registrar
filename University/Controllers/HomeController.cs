b1c4842b4e34bed7260fd58716506dd8f7b73600
zsuzsannamangu <zsuzsannamangu@gmail.com>
2019-05-14 11:03:18 -0700

    add starting files, folders and content

---
+++ University/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using University.Models;
+
+namespace University.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+    }
+}
